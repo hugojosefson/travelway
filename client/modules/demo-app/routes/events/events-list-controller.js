6cf2b72a6ebe4be1de7ac7018117c1278635b931
Hugo Josefson <hugo@josefson.org>
2015-10-03 01:56:54 +0200

    Delete event.

--- client/modules/demo-app/routes/events/events-list-controller.js
+++ client/modules/demo-app/routes/events/events-list-controller.js
@@ -5,6 +5,17 @@ angular.module('demoApp').controller('EventsListController', function (Event) {
 
     Event.query().then(function (events) {
         self.items = events;
+    });
 
-    })
+    self.delete = function (item) {
+        console.log('deleting', item);
+        item.destroy().then(function () {
+            console.log('success deleting');
+            Event.query().then(function (events) {
+                self.items = events;
+            });
+        }, function (reason) {
+            console.error('failed to delete Event', item, reason);
+        });
+    };
 });
